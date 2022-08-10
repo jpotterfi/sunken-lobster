d7bd33a6c46d88f355af420bf4d12337e53b7b28
Jeremy Potterfield <Jeremy.Potterfield@gmail.com>
2022-08-10 14:08:11 -0400

    added to placeCarrier logic

--- src/computerFns/placeCarrier.js
+++ src/computerFns/placeCarrier.js
@@ -8,10 +8,11 @@ function placeCarrier(board) {
 
   while (isFound == false) {
     row = Math.floor(Math.random() * 10);
+    column = Math.floor(Math.random() * 6);
     let orientation = Math.floor(Math.random() * 2);
     isFound = true;
+
     if (orientation == 0 || orientation == 1) {
-      column = Math.floor(Math.random() * 6);
       for (let i = 0; i < length; i++) {
         if (typeof board.coordinates[row][column + i] === "object") {
           isFound = false;
@@ -63,11 +64,52 @@ function placeCarrier(board) {
           if (typeof board.coordinates[row - 2][column + i] === "object") {
             isFound = false;
           }
-        } else if (column == 0) {
+        }
+        if (column == 0) {
           if (typeof board.coordinates[row][column + 1] === "object") {
             isFound = false;
           }
           if (typeof board.coordinates[row][column + 2] === "object") {
+            isFound = false;
+          }
+        } else if (column == 1) {
+          if (typeof board.coordinates[row][column + 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column + 2] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 1] === "object") {
+            isFound = false;
+          }
+        } else if (column >= 2 && column <= 7) {
+          if (typeof board.coordinates[row][column + 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column + 2] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 2] === "object") {
+            isFound = false;
+          }
+        } else if (column == 8) {
+          if (typeof board.coordinates[row][column + 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 2] === "object") {
+            isFound = false;
+          }
+        } else if (column == 9) {
+          if (typeof board.coordinates[row][column - 1] === "object") {
+            isFound = false;
+          }
+          if (typeof board.coordinates[row][column - 2] === "object") {
             isFound = false;
           }
         }
